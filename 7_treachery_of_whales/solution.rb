6366eeb2c107ac6b371ed8f969bcf8d219d9f681
Brent Miller <brent@joinyearone.io>
2021-12-08 10:14:02 -0800

    Day 7 part 2
    Not happy with the performance of part 2, but it is tolerable, so I'll take it.

--- 7_treachery_of_whales/solution.rb
+++ 7_treachery_of_whales/solution.rb
@@ -64,3 +64,50 @@ optimal_position_cost = nil
 end
 
 puts "Most fuel-efficient position is #{optimal_position} with a total cost of #{optimal_position_cost} fuel."
+
+
+# --- Part Two ---
+
+# The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
+
+# As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+
+# As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align them all on; in the example above, this becomes 5:
+
+# Move from 16 to 5: 66 fuel
+# Move from 1 to 5: 10 fuel
+# Move from 2 to 5: 6 fuel
+# Move from 0 to 5: 15 fuel
+# Move from 4 to 5: 1 fuel
+# Move from 2 to 5: 6 fuel
+# Move from 7 to 5: 3 fuel
+# Move from 1 to 5: 10 fuel
+# Move from 2 to 5: 6 fuel
+# Move from 14 to 5: 45 fuel
+# This costs a total of 168 fuel. This is the new cheapest possible outcome; the old alignment position (2) now costs 206 fuel instead.
+
+# Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route! How much fuel must they spend to align to that position?
+
+puts
+puts
+puts "*******************"
+puts "PART II"
+
+optimal_position = nil
+optimal_position_cost = nil
+
+(min_position..max_position).each do |possible_position|
+  cost = 0
+  positions.collect do |position, count|
+    distance = (possible_position - position).abs
+    cost += (1..distance).to_a.sum * count
+  end
+  puts "Cost for position #{possible_position} is #{cost}" if DEBUG
+  if optimal_position.nil? || cost < optimal_position_cost
+    puts "  NEW WINNER" if DEBUG
+    optimal_position = possible_position
+    optimal_position_cost = cost
+  end
+end
+
+puts "Most fuel-efficient position is #{optimal_position} with a total cost of #{optimal_position_cost} fuel."
